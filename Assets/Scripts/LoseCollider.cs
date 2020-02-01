bd9f141bd1d8140f6bb2622090fb20b5edcd6649
chengze <ze.cheng@thoughtworks.com>
2020-02-01 21:27:01 +0800

    base commit

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("trigger enter");
+        SceneManager.LoadScene("Win Screen");
+    }
+}
