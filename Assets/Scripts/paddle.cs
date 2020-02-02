4a1905a84bf53a0f8ce70083ebbfbdea16a7bdd1
chengze <ze.cheng@thoughtworks.com>
2020-02-02 15:07:29 +0800

    make paddle follow mouse movements

---
+++ Assets/Scripts/paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePositionXInUnits = (Input.mousePosition.x / Screen.width) * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(mousePositionXInUnits, transform.position.y);
+        transform.position = paddlePosition;
+    }
+}
