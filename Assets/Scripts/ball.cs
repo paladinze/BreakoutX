15fdd45d3f13352dfb96490d1f11ae48b8130c53
chengze <ze.cheng@thoughtworks.com>
2020-02-09 22:27:55 +0800

    Add random sound effect for ball

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -5,36 +5,51 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-  [SerializeField] Paddle paddle1;
-  [SerializeField] float xSpeed;
-  [SerializeField] float ySpeed;
-  Vector2 paddleToBallVector;
-  Boolean isLaunched = false;
-  // Start is called before the first frame update
-  void Start()
-  {
-    paddleToBallVector = transform.position - paddle1.transform.position;
-  }
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xSpeed;
+    [SerializeField] float ySpeed;
+    [SerializeField] AudioClip[] audioClips;
+    private AudioSource ballAudioSource;
 
-  // Update is called once per frame
-  void Update()
-  {
-    if (!isLaunched) {
-        SetBallAbovePaddle();
+
+    Vector2 paddleToBallVector;
+    Boolean isLaunched = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        ballAudioSource = GetComponent<AudioSource>();
     }
-    LaunchOnMouseClick();
-  }
 
-  private void LaunchOnMouseClick()
-  {
-    if (Input.GetMouseButtonDown(0)) {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, ySpeed);
-        isLaunched = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLaunched) {
+            SetBallAbovePaddle();
+        }
+        LaunchOnMouseClick();
     }
-  }
 
-  private void SetBallAbovePaddle()
-  {
-    transform.position = (Vector2)paddle1.transform.position + paddleToBallVector;
-  }
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, ySpeed);
+            isLaunched = true;
+        }
+    }
+
+    private void SetBallAbovePaddle()
+    {
+        transform.position = (Vector2)paddle1.transform.position + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isLaunched) return;
+        this.GetComponent<AudioSource>().Play();
+
+        // play random sound from collection of audio clips
+        AudioClip audioClip = audioClips[UnityEngine.Random.Range(0, audioClips.Length)];
+        ballAudioSource.PlayOneShot(audioClip);
+    }
 }
