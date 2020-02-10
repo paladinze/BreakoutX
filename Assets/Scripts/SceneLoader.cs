7a0b51595a1bc8be30d3824bce438b4318f2eb55
chengze <ze.cheng@thoughtworks.com>
2020-02-10 22:42:58 +0800

    make score keeping singleton

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private GameStatus gameStatus;
+
     public void LoadNextScene() {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
@@ -12,6 +14,8 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadStartScene() {
         SceneManager.LoadScene(0);
+        gameStatus = FindObjectOfType<GameStatus>();
+        gameStatus.ResetScore();
     }
 
     public void LoadLastScene()
