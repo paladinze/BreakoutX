e11525ff27a892f7ed90aeb601c48d14f43504b5
chengze <ze.cheng@thoughtworks.com>
2020-02-11 21:39:54 +0800

    add scripts

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+
+    [Range(1.0f, 10.0f)][SerializeField] float gameSpeed = 1.0f;
+    [SerializeField] int scorePerBlockDestroyed = 10;
+
+    [SerializeField] int totalScore = 0;
+
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    [SerializeField] bool autoPlayModeEnabled = false;
+
+    private void Awake()
+    {
+        if(FindObjectsOfType<GameStatus>().Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        } else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void IncreaseTotalScore()
+    {
+        totalScore += scorePerBlockDestroyed;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = $"SCORE: {totalScore.ToString()}";
+    }
+
+    public void ResetScore()
+    {
+        Destroy(gameObject);
+        totalScore = 0;
+    }
+
+    public bool GetIsAutoPlaymodeEnabled()
+    {
+        return autoPlayModeEnabled;
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private int totalNumberBlocks = 0;
+    private SceneLoader sceneLoader;
+
+    public void AddTotalBlocksCount()
+    {
+        totalNumberBlocks++;
+    }
+
+    public void DecreaseTotalBlocksCount()
+    {
+        totalNumberBlocks--;
+        if (totalNumberBlocks == 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
