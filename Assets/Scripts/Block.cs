cd81d46d19bf6c976f1b8d2b4b6c7b18d190374f
chengze <ze.cheng@thoughtworks.com>
2020-02-11 13:58:31 +0800

    * Block.prefab: enable maxHits and add vfx on block hit
    * Block.cs:
    * Block.prefab.meta:
    * Wall Block.prefab:
    * Wall Block.prefab.meta:
    * Block Broken 1.png.meta:
    * Block Broken 2.png.meta:

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,32 +1,74 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] Sprite[] blockSprites;
+
     private Level level;
     private GameStatus gameStatus;
-
+    private int hits;
+    private int maxHits;
 
     private void Start()
     {
+        maxHits = blockSprites.Length + 1;
         level = FindObjectOfType<Level>();
-        level.AddTotalBlocksCount();
+        IncreaseBreakableCount();
 
-        gameStatus = FindObjectOfType<GameStatus>();
     }
 
     private void OnCollisionEnter2D (Collision2D collision) {
-        
-       
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        level.RemoveTotalBlocksCount();
 
-        gameStatus.IncreaseTotalScore();
+        if (!tag.Equals("Breakable")) return;
 
+        hits++;
+        if (hits >= maxHits)
+        {
+            DestroyBlock();
+        } else
+        {
+            ShowSpriteForNextDamageLevel();
+        }
+    }
 
+    private void ShowSpriteForNextDamageLevel()
+    {
+        int spriteIndex = hits - 1;
+        Sprite damagedBlockSprite = blockSprites[spriteIndex];
+        if (damagedBlockSprite != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = damagedBlockSprite;
+        } else
+        {
+            Debug.LogError("block damaged sprite is missing");
+        }
+    }
+
+    private void DestroyBlock()
+    {
         Destroy(gameObject);
+        level.DecreaseTotalBlocksCount();
+        gameStatus = FindObjectOfType<GameStatus>();
+        gameStatus.IncreaseTotalScore();
+        CreateExplosion();
+    }
+
+    private void CreateExplosion()
+    {
+        GameObject explosion = Instantiate(explosionVFX, transform.position, transform.rotation);
+        Destroy(explosion, 2f);
+    }
+
+    private void IncreaseBreakableCount()
+    {
+        if (!tag.Equals("Breakable")) return;
+        level.AddTotalBlocksCount();
     }
 
 }
