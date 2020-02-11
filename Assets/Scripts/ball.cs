860c3120b1463da7af702d3a69dffa40835edbf8
chengze <ze.cheng@thoughtworks.com>
2020-02-11 16:31:57 +0800

    add autoplay option  to game status

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -8,9 +8,11 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xSpeed;
     [SerializeField] float ySpeed;
+    [SerializeField] float randomFactor = 0.2f;
     [SerializeField] AudioClip[] audioClips;
-    private AudioSource ballAudioSource;
 
+    private AudioSource ballAudioSource;
+    private Rigidbody2D ballRigidBody2D;
 
     Vector2 paddleToBallVector;
     Boolean isLaunched = false;
@@ -19,6 +21,7 @@ public class Ball : MonoBehaviour
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
         ballAudioSource = GetComponent<AudioSource>();
+        ballRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -33,7 +36,7 @@ public class Ball : MonoBehaviour
     private void LaunchOnMouseClick()
     {
         if (Input.GetMouseButtonDown(0)) {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, ySpeed);
+            ballRigidBody2D.velocity = new Vector2(xSpeed, ySpeed);
             isLaunched = true;
         }
     }
@@ -45,8 +48,15 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // change to velocity on hit (to prevent deterministic reflection ;)
+        Vector2 randomVelocityDelta = new Vector2(
+            UnityEngine.Random.Range(-randomFactor, randomFactor),
+            UnityEngine.Random.Range(-randomFactor, randomFactor));
+
         if (!isLaunched) return;
-        this.GetComponent<AudioSource>().Play();
+
+        // apply random velocity change to ball
+        ballRigidBody2D.velocity += randomVelocityDelta;
 
         // play random sound from collection of audio clips
         AudioClip audioClip = audioClips[UnityEngine.Random.Range(0, audioClips.Length)];
--- Assets/Scripts/paddle.cs
+++ Assets/Scripts/paddle.cs
@@ -6,18 +6,35 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     [SerializeField] float screenWidthInUnits = 16f;
+
+    private GameStatus gameStatus;
+    private Ball ball;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameStatus = FindObjectOfType<GameStatus>();
+        ball = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosXInUnits = (Input.mousePosition.x / Screen.width) * screenWidthInUnits;
-        mousePosXInUnits = Mathf.Clamp(mousePosXInUnits, 0f, screenWidthInUnits);
-        Vector2 paddlePos = new Vector2(mousePosXInUnits, transform.position.y);
+        float xPaddlePos = GetXPosForPaddle();
+        Vector2 paddlePos = new Vector2(xPaddlePos, transform.position.y);
         transform.position = paddlePos;
+    }
+
+    float GetXPosForPaddle()
+    {
+        if (gameStatus.GetIsAutoPlaymodeEnabled())
+        {
+            return ball.transform.position.x;
+        } else
+        {
+            float mousePosXInUnits = (Input.mousePosition.x / Screen.width) * screenWidthInUnits;
+            mousePosXInUnits = Mathf.Clamp(mousePosXInUnits, 0f, screenWidthInUnits);
+            return mousePosXInUnits;
+        }
     }
 }
